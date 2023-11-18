2dc4dd7b948204afda426d772fa0fa41cf70e9db
Clara van Stokkum <vanstcl@whs.school.nz>
2023-11-18 17:44:03 +1300

    father dearest

--- v1/game.js
+++ v1/game.js
@@ -7,7 +7,7 @@
 **/
 console.log("Skiing Adventures!!")
 var ctx
-window.onload=startCanvas
+window.onload = startCanvas
 
 //arrow keys
 const LEFT_ARROW = 37
@@ -20,6 +20,9 @@ const HEIGHT = 475
 //background variables
 var backgroundImage = new Image()
 backgroundImage.src = "background.png"
+
+//turning width
+var turningWidth = 8
 
 //arrow keys
 var leftKeyDown = false
@@ -39,13 +42,13 @@ treeRightImage.src = "tree-right.png"
 const CHARACTER_HEIGHT = 150
 const CHARACTER_WIDTH = 75
 
-var characterMiddleImage = new Image ()
+var characterMiddleImage = new Image()
 characterMiddleImage.src = "character-middle.png"
 
-var characterRightImage = new Image ()
+var characterRightImage = new Image()
 characterRightImage.src = "character-left.png"
 
-var characterLeftImage = new Image ()
+var characterLeftImage = new Image()
 characterLeftImage.src = "character-right.png"
 
 var isAlive = true
@@ -60,14 +63,14 @@ const ROCK_SPEED = 15
 var rockYPosition = 0
 
 
-var rockImage = new Image ()
+var rockImage = new Image()
 rockImage.src = "rock.png"
 
 //checking for keyDown
 function keyDownFunction(keyboardEvent) {
-    var keyDown = keyboardEvent.keyCode
-//check if arrow keys are pressed
-if (keyDown == LEFT_ARROW) {
+  var keyDown = keyboardEvent.keyCode
+  //check if arrow keys are pressed
+  if (keyDown == LEFT_ARROW) {
     leftKeyDown = true
     //console.log("left arrow pressed");
   }
@@ -80,54 +83,54 @@ if (keyDown == LEFT_ARROW) {
 
 //function for keyUp
 function keyUpFunction(keyboardEvent) {
-    var keyUp = keyboardEvent.keyCode  
-    //check if arrow keys are pressed
-    if (keyUp == LEFT_ARROW) {
-      leftKeyDown = false
-      //console.log("left arrow lifted");
-    }
-    if (keyUp == RIGHT_ARROW) {
-      rightKeyDown = false
-    }
+  var keyUp = keyboardEvent.keyCode
+  //check if arrow keys are pressed
+  if (keyUp == LEFT_ARROW) {
+    leftKeyDown = false
+    //console.log("left arrow lifted");
+  }
+  if (keyUp == RIGHT_ARROW) {
+    rightKeyDown = false
+  }
 }
-function startCanvas(){
-    ctx = document.getElementById("myCanvas").getContext("2d")
-    timer = setInterval(updateCanvas, 30)
-    window.addEventListener('keydown', keyDownFunction)
-    window.addEventListener('keyup', keyUpFunction)
+function startCanvas() {
+  ctx = document.getElementById("myCanvas").getContext("2d")
+  timer = setInterval(updateCanvas, 20)
+  window.addEventListener('keydown', keyDownFunction)
+  window.addEventListener('keyup', keyUpFunction)
 }
 
- //generate rocks
- function makeRocks(ROCK_WIDTH, ROCK_HEIGHT){
-  ctx.drawImage(rockImage, Math.random()*100, rockYPosition, ROCK_WIDTH, ROCK_HEIGHT)
+//generate rocks
+function makeRocks(ROCK_WIDTH, ROCK_HEIGHT) {
+  ctx.drawImage(rockImage, Math.random() * 100, rockYPosition, ROCK_WIDTH, ROCK_HEIGHT)
   rockYPosition = rockYPosition + 10
-  
+
 }
 
 
-function updateCanvas(){
-	ctx.clearRect(0,0,WIDTH, HEIGHT)
-    ctx.drawImage(backgroundImage,0,0, WIDTH, HEIGHT);
-    ctx.drawImage(treeLeftImage, 190, -80, TREE_WIDTH, TREE_HEIGHT );
-    ctx.drawImage(treeRightImage, 320, -80, TREE_WIDTH, TREE_HEIGHT );
-    
-    makeRocks(ROCK_WIDTH, ROCK_HEIGHT);
-    //console.log("game started");
+function updateCanvas() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT)
+  ctx.drawImage(backgroundImage, 0, 0, WIDTH, HEIGHT);
+  ctx.drawImage(treeLeftImage, 190, -80, TREE_WIDTH, TREE_HEIGHT);
+  ctx.drawImage(treeRightImage, 320, -80, TREE_WIDTH, TREE_HEIGHT);
 
-    //move character
-    if (leftKeyDown) {
-        characterXPosition -= characterSpeed
-      }
-      if (rightKeyDown) {
-        characterXPosition += characterSpeed
-      }
-    
+  makeRocks(ROCK_WIDTH, ROCK_HEIGHT);
+  //console.log("game started");
+
+  //move character
+  if (leftKeyDown) {
+    characterXPosition -= characterSpeed
+  }
+  if (rightKeyDown) {
+    characterXPosition += characterSpeed
+  }
+
   //show correct character image for left and right
-  
+
   if (leftKeyDown) {
     ctx.drawImage(characterLeftImage, characterXPosition, characterYPosition, CHARACTER_WIDTH + turningWidth, CHARACTER_HEIGHT)
     //console.log("left");
-  } 
+  }
   else if (rightKeyDown) {
     ctx.drawImage(characterRightImage, characterXPosition, characterYPosition, CHARACTER_WIDTH + turningWidth, CHARACTER_HEIGHT)
     //console.log("right");
@@ -136,66 +139,66 @@ function updateCanvas(){
     //console.log("middle");
   }
 
-/****
-**
-** Class definition for the satellites
-**
-****/
-class Satellite {
-  //this.setRandomImage()
-  constructor(images) {
-    this.xPosition = Math.random() * WIDTH
-    this.yPosition = Math.random() * - HEIGHT - ROCK_HEIGHT
-    this.images = images
-    this.setRandomImage()
-  }
-  moveRocks() {
-    this.yPosition += ROCK_SPEED
-    if (this.yPosition > HEIGHT) {
-      this.yPosition = Math.random() * - HEIGHT - ROCK_HEIGHT
+  /****
+  **
+  ** Class definition for the satellites
+  **
+  ****/
+  class Rock {
+    //this.setRandomImage()
+    constructor(images) {
       this.xPosition = Math.random() * WIDTH
+      this.yPosition = Math.random() * - HEIGHT - ROCK_HEIGHT
+      this.images = images
+      this.setRandomImage()
+    }
+    moveRocks() {
+      this.yPosition += ROCK_SPEED
+      if (this.yPosition > HEIGHT) {
+        this.yPosition = Math.random() * - HEIGHT - ROCK_HEIGHT
+        this.xPosition = Math.random() * WIDTH
+      }
+    }
+    setRandomImage() {
+      this.image = this.images[Math.floor(Math.random() * this.images.length)]
     }
   }
-  setRandomImage() {
-    this.image = this.images[Math.floor(Math.random() * this.images.length)]
-  }
-}
 
 
   //keep character in boundaries
-  var bounceBack = 10; 
-  if(characterXPosition < 0 ){
-      characterXPosition += bounceBack;
+  var bounceBack = 10;
+  if (characterXPosition < 0) {
+    characterXPosition += bounceBack;
   }
-  if(characterXPosition > WIDTH - CHARACTER_WIDTH - 40 ){
+  if (characterXPosition > WIDTH - CHARACTER_WIDTH - 40) {
     characterXPosition -= bounceBack;
 
-   
-}
-/****
-**
-** collisions between the rocket and the objects
-**
-****/
-function characterHitRock(rockX, rockY) {
-  var characterHitLeft = characterXPosition
-  var characterHitRight = characterXPosition + CHARACTER_WIDTH
-  var characterHitTop = characterYPosition + 75
-  var characterHitBottom = characterYPosition + 150
 
-
-  var rockHitLeft = rockX
-  var rockHitRight = rockX + ROCK_WIDTH
-  var rockHitTop = rockY
-  var rockHitBottom = rockY + RIGHT_ARROW
-
-  if (characterHitRight > rockHitLeft
-    && characterHitLeft < rockHitRight
-    && characterHitTop < rockHitBottom
-    && characterHitBottom > rockHitTop) {
-    return (true)
-  } else {
-    return (false)
   }
-}
+  /****
+  **
+  ** collisions between the rocket and the objects
+  **
+  ****/
+  function characterHitRock(rockX, rockY) {
+    var characterHitLeft = characterXPosition
+    var characterHitRight = characterXPosition + CHARACTER_WIDTH
+    var characterHitTop = characterYPosition + 75
+    var characterHitBottom = characterYPosition + 150
+
+
+    var rockHitLeft = rockX
+    var rockHitRight = rockX + ROCK_WIDTH
+    var rockHitTop = rockY
+    var rockHitBottom = rockY + RIGHT_ARROW
+
+    if (characterHitRight > rockHitLeft
+      && characterHitLeft < rockHitRight
+      && characterHitTop < rockHitBottom
+      && characterHitBottom > rockHitTop) {
+      return (true)
+    } else {
+      return (false)
+    }
+  }
 } 
